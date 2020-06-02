acf5611db16c04e9236732aa5f13da58df694a0c
Ricardo Watanabe Bento <ricardo@flowhospitalitytraining.co.uk>
2020-06-02 14:35:29 +0100

    c

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    author = models.ForeignKey(User, default=None)
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
